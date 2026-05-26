38225f1e94250dcef0d0cba7e0dec965d5c679ce
kaloyan-tomov <ktomov.dev@gmail.com>
2026-05-26 22:42:09 +0300

    seeding changes and db reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,8 @@
-User.find_or_create_by!(email: "admin@example.com") do |u|
-  u.username = "Admin"
-  u.password = "admin1"
-  u.password_confirmation = "admin1"
-  u.role = :admin
-  u.confirmed_at = Time.current
-end
-
-User.find_or_create_by!(email: "pederakis@manafakis.com") do |u|
-  u.username = "Pederakis Manafakis"
+User.find_or_create_by!(email: "ktomov.dev@gmail.com") do |u|
+  u.username = "Kaloyan"
   u.password = "SPYRALex67"
   u.password_confirmation = "SPYRALex67"
-  u.role = :trainee
-  u.confirmed_at = Time.current
-end
-
-User.find_or_create_by!(email: "maria@example.com") do |u|
-  u.username = "Maria"
-  u.password = "password"
-  u.password_confirmation = "password"
-  u.role = :trainee
+  u.role = :admin
   u.confirmed_at = Time.current
 end
 
